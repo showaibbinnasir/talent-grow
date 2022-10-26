0de5528d281b0a4bb27f344b62e462a36e2eeb98
showaibbinnasir <showaibnishad@gmail.com>
2022-10-26 09:31:24 -0700

    firebase key added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFY1reK-YmP0yx55CI4LPgCfUpH4Rri4s",
+  authDomain: "talentgrow-c94b4.firebaseapp.com",
+  projectId: "talentgrow-c94b4",
+  storageBucket: "talentgrow-c94b4.appspot.com",
+  messagingSenderId: "266322355194",
+  appId: "1:266322355194:web:206b5356ad30a4ecc8ba43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
